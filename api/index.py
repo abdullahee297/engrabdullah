c1a4f7fbc59db264c988f74cf752bf0c2d6affab
Muhammad Abdullah <232841637+abdullahee297@users.noreply.github.com>
2026-04-27 19:03:28 +0500

    Refactor WSGI application import and initialization

--- api/index.py
+++ api/index.py
@@ -1,11 +1,2 @@
-import os
-import sys
-
-path = '/home/yourusername/your-project'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings")
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from portfolio.wsgi import application
+app = application()
