d6b4b78475fc9ebbdeba11723c788df550a8f484
abdullahee297 <232841637+abdullahee297@users.noreply.github.com>
2026-04-28 19:18:11 +0500

    Completed

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import ContactMessage
 
 admin.site.register(ContactMessage)
-=======
-from django.contrib import admin
-from .models import ContactMessage
-
-admin.site.register(ContactMessage)
->>>>>>> bcabad9afcce1d08ec24b6a85be487c198b25228
